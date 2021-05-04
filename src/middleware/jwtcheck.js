72abd2b7eb34b6583405e00e6688d806824dca0f
mnjoyanx <tigranmnjoyan@gmail.com>
2021-05-04 12:56:06 +0400

    add middleware for products

---
+++ src/middleware/jwtcheck.js
@@ -0,0 +1,25 @@
+const { verify } = require('jsonwebtoken')
+require('dotenv').config()
+
+const checkJwt = (req, res, next) => {
+    
+    const { authorization } = req.headers
+    console.log(req.headers)
+    if (!authorization) return res.status(401).send("Access denied. No token provided")
+
+    try {    
+        const token = authorization.split(' ')[1]
+        const decoded = verify(token, process.env.JWT_SECRET) 
+        req.user = decoded
+        next()
+        
+    } catch (err) {
+        return res.status(401).json({
+            message: "Not Authorized"
+        })    
+    }
+
+}
+
+
+module.exports = checkJwt
--- src/routes/products.js
+++ src/routes/products.js
@@ -1,9 +1,10 @@
 const router = require('express-promise-router')()
+const jwtCheck = require('../middleware/jwtcheck') 
 
 const { productsController } = require('../controllers')
 
 router.route('/:id').get(productsController.getOne)
-router.route('/').get(productsController.getAll)
+router.route('/').get(jwtCheck, productsController.getAll)
 router.route('/').post(productsController.create)
 router.route('/:id').put(productsController.update)
 router.route('/:id').delete(productsController.delete)  
